2cbe137a1e68c6227ade5e4e3a073852a75f730c
Beaming_Deamon <51133964+beamingdeamon@users.noreply.github.com>
2021-10-05 20:27:46 +0600

    Multicast_server update

--- Multicast/multicast_server/multicast_server/Program.cs
+++ Multicast/multicast_server/multicast_server/Program.cs
@@ -12,18 +12,25 @@ namespace multicast_server
     {
         static void Main(string[] args)
         {
-            Socket server = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
-            IPEndPoint endPoint = new IPEndPoint(IPAddress.Any, 1024);
-            server.Bind(endPoint);
-            //224.5.5.5 потому что если выйдет за пределы с 224 - 239 не будет работать
-            IPAddress ip = IPAddress.Parse("224.5.5.5");
-            server.SetSocketOption(SocketOptionLevel.IP, SocketOptionName.AddMembership, new MulticastOption(ip, IPAddress.Any));
-            byte[] data = new byte[1024];
-            server.Receive(data);
+            while (true)
+            {
+                Socket server = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+                IPEndPoint endPoint = new IPEndPoint(IPAddress.Any, 1024);
+                server.Bind(endPoint);
+                //224.5.5.5 потому что если выйдет за пределы с 224 - 239 не будет работать
+                IPAddress ip = IPAddress.Parse("224.5.5.5");
+                server.SetSocketOption(SocketOptionLevel.IP, SocketOptionName.AddMembership, new MulticastOption(ip, IPAddress.Any));
+                byte[] data = new byte[1024];
+                server.Receive(data);
 
-            string message = Encoding.UTF8.GetString(data);
-            Console.WriteLine(message);
-            server.Close();
+                string message = Encoding.UTF8.GetString(data);
+                Console.WriteLine(message);
+                if (message.IndexOf("break") > -1)
+                {
+                    break;
+                    server.Close();
+                }
+            }
         }
     }
 }
